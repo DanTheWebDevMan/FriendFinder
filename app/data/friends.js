4b6c487818f5a46fa74f329c0cf77d84c2f3f5d9
Danniel Zelaya <dannielzelaya@gmail.com>
2019-08-23 19:58:29 -0500

    html routes created and profile arrays created in friends.js file

---
+++ app/data/friends.js
@@ -0,0 +1,74 @@
+var friends = [
+    {
+    "name":"Sonia",
+    "photo": "https://solarsystem.nasa.gov/system/basic_html_elements/x11561_Sun.png.pagespeed.ic.0igPUXQBpc.png",
+    "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+    },
+
+    {
+    "name":"Luna",
+    "photo": "https://gallery.yopriceville.com/var/resizes/Free-Clipart-Pictures/Sun-and-Moon-PNG/Cartoon_Moon_with_Stars_PNG_Clip_Art_Image.png?m=1507172110",
+    "scores":[
+        2,
+        1,
+        5,
+        1,
+        3,
+        3,
+        4,
+        5,
+        2,
+        2
+    ]
+    },
+
+    {
+    "name":"Stella",
+    "photo": "https://banner2.kisspng.com/20180817/sea/kisspng-clip-art-animated-film-image-star-cartoon-5b770e66e58e11.8296414215345291269403.jpg",
+    "scores":[
+        5,
+        4,
+        4,
+        1,
+        4,
+        1,
+        5,
+        1,
+        3,
+        3
+    ]
+    },
+
+    {  
+    "name":"Terra",
+    "photo": "http://www.inspiremykids.com/wp-content/uploads/2011/04/smiling-planet-earth-cartoon-2-thum.jpg",
+    "scores":[
+        5,
+        4,
+        4,
+        1,
+        4,
+        1,
+        5,
+        1,
+        3,
+        3
+    ]
+    },
+
+module.exports = friends;
+
+  
+
+]
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,6 @@
+var friends = require('..data/friends');
+
+module.exports = functio(app) {
+    //return friends from friends.js as JSON
+    app.get("/api/friends")
+}
