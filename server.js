9e4ce5da1d5099dfa8961f779942c26dc2756ee3
Danniel Zelaya <dannielzelaya@gmail.com>
2019-08-24 17:43:32 -0500

    created heroku environment

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 // Link in html and api routes
-var apiRoutes = require('./app/routing/api-routes.js');
-var htmlRoutes = require('./app/routing/html-routes.js');
+var apiRoutes = require('app/routing/api-routes.js');
+var htmlRoutes = require('app/routing/html-routes.js');
 
 // Set up Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
