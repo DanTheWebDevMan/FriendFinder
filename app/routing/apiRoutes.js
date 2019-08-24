6546d10e550c1942717a5619b9ec1b58e93c4d17
Danniel Zelaya <dannielzelaya@gmail.com>
2019-08-23 20:28:06 -0500

    corrected function code in server.js file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 var friends = require('..data/friends');
 
-module.exports = functio(app) {
+module.exports = function(app) {
     //return friends from friends.js as JSON
     app.get("/api/friends")
-}
+};
